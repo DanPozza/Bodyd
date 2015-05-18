210e28e7b7a20450bf0b1c2095486569897d2b24
DanPozza <danpozza21@hotmail.it>
2015-05-18 12:14:01 +0200

    Update BodyDetection.cpp

--- BodyDetection.cpp
+++ BodyDetection.cpp
@@ -1,128 +1,11 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-/*#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "opencv2/imgcodecs/imgcodecs.hpp"
-#include "opencv2/videoio/videoio.hpp"
-#include "opencv2/video/video.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/opencv.hpp"
-#include <sstream>
 #include <iostream>
 #include <stdio.h>
-#include<stdlib.h>
-#include<libavformat/avformat.h>
-#include<libavcodec/avcodec.h>
+#include <fstream>
 
-using namespace std;
-using namespace cv;
-
-
-
-void detection(Mat frame);
-
-
-String body_cascade_name = "/home/ubuntu/opencv/opencv-3.0.0-alpha/data/haarcascades/haarcascade_frontalface_alt.xml";
-CascadeClassifier body_cascade;
-string window_name = "Capture - body detection";
-//RNG rng(12345);
-
-
-int main(int argc, char* argv[])
-{
-	/*VideoCapture cap(0); // open the default camera
-	    if(!cap.isOpened())  // check if we succeeded
-	        return -1;
-
-	    Mat edges;
-	    namedWindow("edges",1);
-	    for(;;)
-	    {
-	        Mat frame;
-	        cap >> frame; // get a new frame from camera
-	        cvtColor(frame, edges, COLOR_BGR2GRAY);
-	        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-	        Canny(edges, edges, 0, 30, 3);
-	        imshow("edges", edges);
-	        if(waitKey(30) >= 0) break;
-	    }
-	    // the camera will be deinitialized automatically in VideoCapture destructor
-	    return 0;*//*
-
-	VideoCapture vcap;
-	Mat image;    
-	const string videoStreamAddress = "http://192.168.1.134:8082";
-
-	//open the video stream and make sure it's opened
-
-	if (!body_cascade.load(body_cascade_name))
-	{
-		printf("--(!)Error loading\n"); return -1;
-	}
-	vcap.open(videoStreamAddress);
-
-
-	if (!vcap.isOpened())
-	{
-		cout << "Error opening video stream or file " <<endl;
-		return -1;
-	}
-        bool result;
-	while (1)
-	{
-		if (!vcap.read(image))
-		{
-			cout << "No frame" << endl;
-			waitKey(0);
-		}
-	      result=	detection(image);
-	      if (result == true){cout<<"detected"<<endl;}
-	      else {cout<<"nobody"<<endl;}
-	  
-	
-
-	}
-
-
-
-}
-
-//######################################################################################à
-bool detection(Mat frame)
-{
-	vector<Rect> bodys;
-	Mat frame_gray;
-
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-
-
-    body_cascade.detectMultiScale(frame, bodys, 1.3, 2, 3, Size(30, 30));
-
-
-	for (unsigned int j = 0; j < bodys.size(); j++)
-	{
-		Point center(bodys[j].x + bodys[j].width*0.5, bodys[j].y + +bodys[j].height*0.5);
-		ellipse(frame, center, Size(bodys[j].width*0.5, bodys[j].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-	}
-        if(bodys != NULL){return true;}
-       else{	return false;}
-}*/
-
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "opencv2/imgcodecs/imgcodecs.hpp"
-#include "opencv2/videoio/videoio.hpp"
-#include "opencv2/video/video.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/opencv.hpp"
-#include <sstream>
-#include <iostream>
-#include <stdio.h>
-#include<stdlib.h>
-//#include<libavformat/avformat.h>
-//#include<libavcodec/avcodec.h>
 
 using namespace std;
 using namespace cv;
@@ -132,51 +15,71 @@ using namespace cv;
 bool detection(Mat frame);
 
 /** Global variables*/
-String body_cascade_name = "/home/ubuntu/opencv/opencv-3.0.0-alpha/data/haarcascades/haarcascade_frontalface_alt.xml";
+String body_cascade_name = "C:/opencv/sources/data/haarcascades/haarcascade_upperbody.xml";
 CascadeClassifier body_cascade;
 string window_name = "Capture - body detection";
 //RNG rng(12345);
 
 
+
 int main(int argc, char* argv[])
 {
-	/*VideoCapture cap(0); // open the default camera
-	    if(!cap.isOpened())  // check if we succeeded
-	        return -1;
-	    Mat edges;
-	    namedWindow("edges",1);
-	    for(;;)
-	    {
-	        Mat frame;
-	        cap >> frame; // get a new frame from camera
-	        cvtColor(frame, edges, COLOR_BGR2GRAY);
-	        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-	        Canny(edges, edges, 0, 30, 3);
-	        imshow("edges", edges);
-	        if(waitKey(30) >= 0) break;
-	    }
-	    // the camera will be deinitialized automatically in VideoCapture destructor
-	    return 0;*/
+	/*CvCapture* capture;
+	Mat frame;
 
-	VideoCapture vcap;
-	Mat image;
-	const string videoStreamAddress = "http://10.20.37.18:8082/mjpg/video.mjpg";
+
+	if (!body_cascade.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt2.xml"))
+	{
+		printf("--(!)Error loading\n"); return -1;
+	};
+
+	capture = cvCaptureFromCAM(0);
+	
+
+	if (capture != NULL)
+	{
+		while (1)
+		{
+			frame = cvQueryFrame(capture);
+			if (!frame.empty())
+			{
+				detection(frame);
+			}
+			else {
+				printf(" --(!) No captured frame -- Break!");
+				break;
+			}
+			int d = waitKey(100);
+			if ((char)d == 'd')
+			{
+				break;
+			}
+		}
+	}
+	return 1;*/
+
+	cv::VideoCapture vcap;
+	cv::Mat image;    // This works on a D-Link CDS-932L
+	//const std::string videoStreamAddress = "http://192.168.1.121:8082";
 
 	//open the video stream and make sure it's opened
 
-	if (!body_cascade.load(body_cascade_name))
+	if (!body_cascade.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt2.xml"))
 	{
 		printf("--(!)Error loading\n"); return -1;
 	}
-	vcap.open(videoStreamAddress);
 
-
-	if (!vcap.isOpened())
+	if (!vcap.open(0))
 	{
-		cout << "Error opening video stream or file " <<endl;
+		std::cout << "Error opening video stream or file" << std::endl;
 		return -1;
 	}
-        bool result;
+
+	bool result;
+	char x[50];
+	string z;
+	bool detect =true;
+
 	while (1)
 	{
 		if (!vcap.read(image))
@@ -184,17 +87,35 @@ int main(int argc, char* argv[])
 			cout << "No frame" << endl;
 			waitKey(0);
 		}
-	      result=	detection(image);
-	      if (result == true){cout<<"detected"<<endl;}
-	      else {cout<<"nobody"<<endl;}
+		result = detection(image);
+		if ((result == true) && detect==true)
+		{ 
+			cout << "detected" << endl;
+			sprintf(x, "curl http://localhost:3000/2/2"); // 2/2 rappresentano la latitudine e longitudine della videocamera
+			z.assign(x);
+			system(z.c_str());
+			detect = false;
 
-
-
+		}
+		if (detect==false)
+		{
+			int i = 0;
+			////creiamo un tempo d'attesa di 5 minuti prima di inviare un'altra mail in presenza di persone
+			for (i; i < 9000;i++)
+			{
+				vcap.read(image);
+					imshow(window_name, image);
+					waitKey(10);
+			}
+			detect = true;
+		}
+		else
+		{
+			cout << "nobody" << endl;
+		}
 	}
-
-
-
-}
+		
+	}
 
 //######################################################################################à
 bool detection(Mat frame)
@@ -202,19 +123,26 @@ bool detection(Mat frame)
 	vector<Rect> bodys;
 	Mat frame_gray;
 
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	cvtColor(frame, frame_gray, CV_BGR2GRAY);
 	equalizeHist(frame_gray, frame_gray);
+	/**detect body */
 
+	body_cascade.detectMultiScale(frame_gray, bodys, 1.8, 2, 3, Size(30, 30));
 
-    body_cascade.detectMultiScale(frame, bodys, 1.3, 2, 3, Size(30, 30));
-
-
+	/**draw ellipse*/
 	for (unsigned int j = 0; j < bodys.size(); j++)
 	{
 		Point center(bodys[j].x + bodys[j].width*0.5, bodys[j].y + +bodys[j].height*0.5);
 		ellipse(frame, center, Size(bodys[j].width*0.5, bodys[j].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
 	}
-        if(bodys.empty()){return false;}
-       else{	return true;}
+
+
+	imshow(window_name, frame);
+	
+	if (bodys.empty()){ waitKey(10); return false; }
+	else{ waitKey(1000); return true; }
 }
+
+
+
 
